a089e8ea3c7b1bb7c2c897b2af80ea5b3a413fac
Jaap H. Abbring <j.h.abbring@tilburguniversity.edu>
2020-11-13 09:06:39 +0100

    Update dynamicDiscreteChoice.m

--- dynamicDiscreteChoice.m
+++ dynamicDiscreteChoice.m
@@ -1,6 +1,6 @@
 %{
 \documentclass{article}
-	\title{Dynamic Discrete Choice Models: Methods, Matlab Code, and Exercises\thanks{We wish to thank Jeffrey R. Campbell for help with \textsc{Komments++} and Nan Yang and our students for comments on earlier versions of this code. First draft: February 8, 2011. \copyright 2020 Jaap H. Abbring and Tobias J. Klein.}}
+	\title{Dynamic Discrete Choice Models: Methods, Matlab Code, and Exercises\thanks{We thank Jeff Campbell for help with \textsc{Komments++} and Nan Yang and our students for comments on earlier versions of this code. First draft: February 8, 2011. \copyright 2020 Jaap H. Abbring and Tobias J. Klein.}}
 	\author{Jaap H. Abbring\thanks{Department of Econometrics \& OR, Tilburg University, P.O. Box
 		90153, 5000 LE Tilburg, The Netherlands. E-mail: \url{mailto:jaap@abbring.org}{jaap@abbring.org}. Web: \url{http://jaap.abbring.org/}{jaap.abbring.org}.}}
 			\author{Tobias J. Klein\thanks{Department of Econometrics \& OR, Tilburg University, P.O. Box
